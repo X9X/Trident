68cf911f28ea2d86c783a05b3514ebcf86bbdafd
Young <yangjinbao02@meituan.com>
2017-03-29 17:27:12 +0800

    config webpack

--- tools/chalkConfig.js
+++ tools/chalkConfig.js
@@ -1,6 +1,8 @@
 // Centralized configuration for chalk, which is used to add color to console.log statements.
-import chalk from 'chalk';
-export const chalkError = chalk.red;
-export const chalkSuccess = chalk.green;
-export const chalkWarning = chalk.yellow;
-export const chalkProcessing = chalk.blue;
+const chalk = require('chalk');
+module.exports = {
+  chalkError : chalk.red,
+  chalkSuccess : chalk.green,
+  chalkWarning : chalk.yellow,
+  chalkProcessing : chalk.blue
+};
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,18 +1,18 @@
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import autoprefixer from 'autoprefixer';
-import path from 'path';
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const  path = require('path');
 
-export default {
+module.exports = {
   resolve: {
     extensions: ['*', '.js', '.jsx', '.json']
   },
-  devtool: 'eval-source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
+  devtool: 'eval-source-map',
   entry: [
     // must be first entry to properly set public path
     './src/webpack-public-path',
     'webpack-hot-middleware/client?reload=true',
-    path.resolve(__dirname, 'src/index.js') // Defining path seems necessary for this to work consistently on Windows machines.
+    path.resolve(__dirname, 'src/index.js')
   ],
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
@@ -50,7 +50,7 @@ export default {
   ],
   module: {
     rules: [
-      {test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader']},
+      {test: /\.js|jsx$/, exclude: /node_modules/, loader: 'babel-loader'},
       {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader'},
       {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff'},
       {test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
